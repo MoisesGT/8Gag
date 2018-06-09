a48cc5890aa9ae5c559c009896b38973a38f6f01
Moises <gmoises577@gmail.com>
2018-06-08 23:28:15 -0600

    Primer commit, Vistas y conexion a firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,46 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { SubirPage } from '../pages/subir/subir';
+
+//firebase
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAljWVA5R9-onznuIhhWDFDZTkkV5vtnKc",
+  authDomain: "gag-c70f2.firebaseapp.com",
+  databaseURL: "https://gag-c70f2.firebaseio.com",
+  projectId: "gag-c70f2",
+  storageBucket: "gag-c70f2.appspot.com",
+  messagingSenderId: "307837438249"
+};
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    SubirPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    SubirPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    AngularFireDatabase,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
